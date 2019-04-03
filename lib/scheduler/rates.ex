10c0d184559853a8df007835fcab1824f57552df
László Horváth <bedafoltyn@gmail.com>
2019-04-03 22:53:58 +0200

    rate scheduler did_grow function added

--- lib/scheduler/rates.ex
+++ lib/scheduler/rates.ex
@@ -31,9 +31,23 @@ defmodule Scheduler.Rates do
         current.currency === rate.currency
       end)
 
+      current_with_grow = current_rate
+      |> Map.put(:did_grow, did_grow(current_rate.value, rate.value, rate.did_grow))
+
       if current_rate do
-        Rate.Service.update(rate, current_rate)
+        Rate.Service.update(rate, current_with_grow)
       end
     end)
+  end
+
+  defp did_grow(new_value, old_value, old_grow) do
+    cond do
+      old_value == new_value ->
+        old_grow
+      old_value < new_value ->
+        true
+      old_value > new_value ->
+        false
+    end
   end
 end
